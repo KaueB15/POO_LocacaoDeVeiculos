0ea3793e9529603c4ffe13adeb4a2c23a0af1fff
Kaue Batista <kauecardozo3@gmail.com>
2023-11-27 14:38:45 -0300

    VeiculoDao Criada

--- src/br/edu/fescfafic/LocacaoDeVeiculos/Dao/VeiculosDao.java
+++ src/br/edu/fescfafic/LocacaoDeVeiculos/Dao/VeiculosDao.java
@@ -1,4 +1,32 @@
 package br.edu.fescfafic.LocacaoDeVeiculos.Dao;
 
-public class VeiculosDao {
+import br.edu.fescfafic.LocacaoDeVeiculos.Interfaces.IDao;
+import br.edu.fescfafic.LocacaoDeVeiculos.Model.Veiculos.Veiculo;
+
+import java.util.ArrayList;
+
+public class VeiculosDao implements IDao<Veiculo> {
+    ArrayList<Veiculo> listaVeiculos = new ArrayList<>();
+
+    @Override
+    public void listarTodos() {
+        for(Veiculo veiculo : listaVeiculos){
+            System.out.println(veiculo.getPlacaDoVeiculo());
+        }
+    }
+
+    @Override
+    public void adicionar(Veiculo veiculo){
+        listaVeiculos.add(veiculo);
+    }
+
+    @Override
+    public void remover() {
+
+    }
+
+    @Override
+    public void buscar() {
+
+    }
 }
