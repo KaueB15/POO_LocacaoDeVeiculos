8ee39ea212b183591a3d7b7ffb23aff9c9cc9c4d
Kaue Batista <kauecardozo3@gmail.com>
2023-11-29 15:18:47 -0300

    Veiculo Controller

---
+++ src/br/edu/fescfafic/LocacaoDeVeiculos/Controller/VeiculoController.java
@@ -0,0 +1,48 @@
+package br.edu.fescfafic.LocacaoDeVeiculos.Controller;
+
+import br.edu.fescfafic.LocacaoDeVeiculos.Dao.VeiculosDao;
+import br.edu.fescfafic.LocacaoDeVeiculos.Exceptions.VeiculoNaoEncontradoException;
+import br.edu.fescfafic.LocacaoDeVeiculos.Interfaces.IController;
+import br.edu.fescfafic.LocacaoDeVeiculos.Model.Veiculos.Veiculo;
+
+import java.util.ArrayList;
+
+public class VeiculoController implements IController<Veiculo> {
+    public VeiculosDao dao;
+
+    public VeiculoController(){
+        this.dao = new VeiculosDao();
+    }
+
+
+    @Override
+    public void remover(String placa) {
+        boolean encontrado = false;
+        try {
+            for (Veiculo veiculo : dao.listaVeiculos) {
+                if (veiculo.getPlacaDoVeiculo().equals(placa)) {
+                    dao.remover(veiculo);
+                    System.out.println("Veiculo Removido com sucesso");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if(!encontrado){
+                throw new VeiculoNaoEncontradoException();
+            }
+        }catch (VeiculoNaoEncontradoException e) {
+            System.err.println(e);
+        }
+    }
+
+    @Override
+    public void adicionar(Veiculo veiculo) {
+        dao.adicionar(veiculo);
+        System.out.println("Veiculo adicionado");
+    }
+
+    @Override
+    public ArrayList<Veiculo> listar() {
+        return dao.listar();
+    }
+}
